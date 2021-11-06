5e5922e1d79cf306b43b66036ba57f4b83de95d2
Drake62610 <drake@DESKTOP-LJ8P0SK.localdomain>
2021-11-06 02:45:53 +0100

    Add focus feature and first collision test

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    // GameObjects
     public GameObject bulletPrefab;
+    public GameObject hitBox;
+    
+    // Parameters
+    public float speed;
     public float fireDelay;
+    public float focusModifier;
+    
+    //Locals
+    float activeModifier = 1;
     float cooldown=0;
 
     // Start is called before the first frame update
@@ -18,13 +27,24 @@ public class PlayerBehaviour : MonoBehaviour
     void Update()
     {
         cooldown -= Time.deltaTime;
-        transform.Translate(0, Input.GetAxis("Vertical") * 5 * Time.deltaTime, 0);
-        transform.Translate(Input.GetAxis("Horizontal") * 5 * Time.deltaTime, 0, 0);
+        transform.Translate(0, Input.GetAxis("Vertical") * speed * Time.deltaTime * activeModifier, 0);
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime* activeModifier, 0, 0);
 
         if (Input.GetAxis("Fire1") == 1 && cooldown < 0 )
         {
             Instantiate(bulletPrefab, transform.position, transform.rotation);
             cooldown = fireDelay;
+        }
+
+        if (Input.GetAxis("Focus") == 1)
+        {
+            hitBox.SetActive(true);
+            activeModifier = focusModifier;
+        }
+        else
+        {
+            hitBox.SetActive(false);
+            activeModifier = 1;
         }
     }
 }
