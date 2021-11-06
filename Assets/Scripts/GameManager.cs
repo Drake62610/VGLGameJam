da1b2a7e5c842aef4333536f68b8b64cfe006323
Drake62610 <drake@DESKTOP-LJ8P0SK.localdomain>
2021-11-06 19:13:07 +0100

    Push Gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
+    public GameObject continueEvent;
     public static GameManager instance = null;
 
     public float playerHealth;
@@ -22,11 +23,15 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    private void triggerContinue() {
+        Time.timeScale = 0;
+        continueEvent.SetActive(true);
     }
 }
