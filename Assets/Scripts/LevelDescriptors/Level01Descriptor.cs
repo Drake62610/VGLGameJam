1febe98cea8da81af1024427534f806a0130917e
Sugiuro <sugiuro.dev@gmail.com>
2021-11-06 16:55:42 +0100

    Simplified enemyspawn system

--- Assets/Scripts/LevelDescriptors/Level01Descriptor.cs
+++ Assets/Scripts/LevelDescriptors/Level01Descriptor.cs
@@ -12,24 +12,16 @@ public class Level01Descriptor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // EnemiesSpawnData.Add(new EnemySpawnData(new Vector3(2, 2, 0), 3.0f, enemyGameObject));
-        // EnemiesSpawnData.Add(new EnemySpawnData(new Vector3(4, 3, 0), 4.0f, enemyGameObject));
-        // EnemiesSpawnData.Add(new EnemySpawnData(new Vector3(3, 5, 0), 2.0f, enemyGameObject));
+        foreach (var enemySpawnData in enemiesSpawnData)
+        {
+            StartCoroutine(SpawnEnemy(enemySpawnData));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator SpawnEnemy(EnemySpawnData enemySpawnData)
     {
-        var enemiesSpawnDataToSpawn = enemiesSpawnData.FindAll((enemySpawnData) => enemySpawnData.spawnTime <= Time.timeSinceLevelLoad);
-        if (enemiesSpawnDataToSpawn.Count > 0)
-        {
-            foreach (var enemySpawnDataToSpawn in enemiesSpawnDataToSpawn)
-            {
-                var enemyGameObject = Instantiate(enemySpawnDataToSpawn.enemyGameObject, enemySpawnDataToSpawn.spawnPosition + gameField.transform.position, Quaternion.identity);
-                enemyGameObject.transform.parent = gameField.transform;
-            }
-            Debug.Log("o/");
-            enemiesSpawnData.RemoveAll(x => enemiesSpawnDataToSpawn.Contains(x));
-        }
+        yield return new WaitForSeconds(enemySpawnData.spawnTime);
+        var enemyGameObject = Instantiate(enemySpawnData.enemyGameObject, enemySpawnData.spawnPosition + gameField.transform.position, Quaternion.identity);
+        enemyGameObject.transform.parent = gameField.transform;
     }
 }
