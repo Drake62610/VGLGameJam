a7ad57f5eb89909de8657f871763a9d4eb7cc07b
Drake62610 <drake@DESKTOP-LJ8P0SK.localdomain>
2021-11-07 21:18:35 +0100

    ugly planet

--- Assets/Scripts/Enemies/AIs/BulletBehavior/ParametrableLaserBulletBehavior.cs
+++ Assets/Scripts/Enemies/AIs/BulletBehavior/ParametrableLaserBulletBehavior.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class ParametrableLaserBulletBehavior : IParametrableBullet
 {
     void Start() {
-        
+        transform.rotation = Quaternion.Euler(0, 0, Vector3.Angle(direction, new Vector3(0,1,0)));
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {  
+        transform.rotation = Quaternion.Euler(0, 0, Vector3.Angle(direction, new Vector3(0,1,0)));
         transform.position += direction.normalized * speed * Time.deltaTime;
         Destroy(this.gameObject, ttl);
     }
