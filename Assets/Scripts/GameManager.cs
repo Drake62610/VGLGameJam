60e695a62c1e719b95b29df4f8d535c91d1b47db
yarimoto <alexisguys@gmail.com>
2021-11-08 02:38:34 +0100

    Au dodo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,7 @@ public class GameManager : MonoBehaviour
     private GameObject continueEvent;
     private GameObject levelLoader;
     private bool loadLevel = true;
+    private int level = 1;
 
 
     private void Awake()
@@ -53,7 +54,11 @@ public class GameManager : MonoBehaviour
 
     public void ChangeLevel()
     {
-        levelLoader.BroadcastMessage("ChangeLevel", SceneManager.GetActiveScene().buildIndex + 1);
+        level += 1;
+        if (level == 2)
+            levelLoader.BroadcastMessage("ChangeLevel", SceneManager.GetActiveScene().buildIndex + 1);
+        else
+            GameOver();
     }
 
     public void TriggerContinue()
